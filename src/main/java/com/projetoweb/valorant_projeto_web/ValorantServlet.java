58495eb1efb486dc3c4ee20cc00c8db95590349a
Kelvin Vasconcelos <kelvinvasconcelos@gmail.com>
2021-07-08 19:19:55 -0300

    :tada: Feito verbos doGet e doPost.

--- src/main/java/com/projetoweb/valorant_projeto_web/ValorantServlet.java
+++ src/main/java/com/projetoweb/valorant_projeto_web/ValorantServlet.java
@@ -27,11 +27,28 @@ public class ValorantServlet extends HttpServlet{
             resp.setContentType("text/html; charset=utf-8");
 
             Gson gson = new Gson();
+            AgentesResponse agente = gson.fromJson(response.body().string(), AgentesResponse.class);
+            
+            resp.getWriter().println("<h1>"+ (agente.getData().get(2).getDisplayName()) +"</h1>");
+            resp.getWriter().println("<img src="+ (agente.getData().get(2).getDisplayIcon()) +" width='100' height='100'>");
+            resp.getWriter().println("<p>"+ (agente.getData().get(2).getDescription()) +"</p>");
+        }
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request.Builder requestBuilder = new Request.Builder().url("https://valorant-api.com/v1/agents?language=pt-BR");
+        Request request = requestBuilder.build();
+        try (Response response = client.newCall(request).execute()){
+            resp.addHeader("Content-type", "application/json; charset=utf-8");
+            resp.setContentType("text/html; charset=utf-8");
+
+            Gson gson = new Gson();
             AgentesResponse agentes = gson.fromJson(response.body().string(), AgentesResponse.class);
             
-            resp.getWriter().println("<h1>"+ (agentes.getData().get(2).getDisplayName()) +"</h1>");
-            resp.getWriter().println("<img src="+ (agentes.getData().get(2).getDisplayIcon()) +" width='100' height='100'>");
-            resp.getWriter().println("<p>"+ (agentes.getData().get(2).getDescription()) +"</p>");
+            resp.getWriter().println(gson.toJson(agentes.getData()));
         }
     }
 }
